1bacfb2d9f8968a11c5f2efb08e29f14de49f704
waniln <okim2002a@gmail.com>
2026-04-27 22:11:43 +0900

    feat: 입력값 유효성 검사 추가

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
 const authMiddleware = require('../middlewares/authMiddleware');
+const { productValidator } = require('../middlewares/validators');
 
-router.post('/',authMiddleware, productController.createProduct);
+router.post('/',authMiddleware, productValidator, productController.createProduct);
 router.get('/', productController.getAllProducts);
 router.get('/:id', productController.getProductById);
 router.put('/:id', authMiddleware, productController.updateProudct);
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const userConroller = require('../controllers/userController');
+const { registerValidator, loginValidator } = require('../middlewares/validators');
 
-router.post('/register', userConroller.register);
-router.post('/login', userConroller.login);
+router.post('/register', registerValidator, userConroller.register);
+router.post('/login', loginValidator, userConroller.login);
 
 module.exports = router;
